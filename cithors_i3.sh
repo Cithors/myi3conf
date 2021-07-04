17d26a51a6828bea2a882bb3ba2147bc200fa39d
Cithors <hostin.christopher@gmail.com>
2021-07-04 22:37:59 +0200

    script v1

--- cithors_i3.sh
+++ cithors_i3.sh
@@ -1,4 +1,10 @@
-apt install i3 git feh autoconf compton libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev gcc -y
+origin = $PWD
+
+add-apt-repository ppa:kgilmer/speed-ricer -y
+
+apt update
+
+apt install polybar i3 git feh make compton libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev gcc -y
 
 mkdir i3-gaps && cd i3-gaps
 
@@ -11,7 +17,7 @@ autoreconf -fi ..
 ../configure
 make -j8 install
 
-cd ../../..
+cd $origin
 
 cp -r {i3,polybar,compton.conf} ~/.config
 
